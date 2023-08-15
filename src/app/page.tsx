9a8c3cb63fb23299494ab494183392bfc6bfca81
Laylson Sousa <laylsons3@gmail.com>
2023-08-14 23:02:16 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,66 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="bg-indigo-800 w-full h-screen">
+        <header className="bg-indigo-100/40 h-32 w-full flex justify-center">
+          <div className="max-w-4xl flex justify-between items-center w-full">
+            <h1 className="text-3xl font-bold">CABEÇALHO</h1>
+            <nav>
+              <ul className="flex gap-x-8">
+                <li>Inicio</li>
+                <li>Projetos</li>
+                <li>Itens</li>
+                <li>Contato</li>
+                <li>Sobre</li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <div className="w-full h-full flex mt-32 justify-center">
+          <div className="p-4 rounded-lg w-full max-w-4xl h-[200px]">
+            <div className="flex flex-col text-white">
+              <h1 className="text-7xl font-black">
+                Facilitando seu acesso aos Benefícios Fiscais
+              </h1>
+              <p>
+                Assessoria jurídica tributária focada nos benefícios fiscais da
+                MP 2.199-14/2001 (Redução de 75% do IRPJ e Reinvestimento)
+              </p>
+              <a
+                href="#"
+                className="bg-white px-1 py-1 rounded shadow-inner shadow-zinc-400 text-zinc-800"
+              >
+                Saiba como
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <section className="h-screen bg-orange-800 flex justify-center items-center">
+        <div className="flex flex-col max-w-4xl w-full">
+          <h1 className="text-lg font-bold">QUEM SOMOS</h1>
+          <p>
+            A AV ADVOCACIA ESTRATÉGICA opera na área de atuação da
+            Superintendência do Desenvolvimento do Nordeste (Sudene) no que diz
+            respeito aos benefícios fiscal da Redução de 75% (setenta e cinco
+            por cento) do imposto sobre a renda e adicionais calculados com base
+            no lucro da exploração e do benefício fiscal do Reinvestimento. O
+            diferencial do escritório é a expertise da equipe. A sua localização
+            estratégica também facilita a diligência presencial na própria sede
+            da Sudene, com a finalidade de resguardar os interesses dos seus
+            clientes com a maior brevidade possível. A AV ADVOCACIA ESTRATÉGICA
+            ao longo dos últimos anos aprovou na SUDENE dezenas de pleitos de
+            Redução de 75% do IRPJ sobre o lucro da exploração em
+            empreendimentos nos setores de infraestrutura de energia; de
+            turismo, considerando os empreendimentos hoteleiros; da agricultura
+            irrigada-fruticultura; da indústria de transformação nos setores de
+            alimentos e bebidas, produtos farmacêuticos; minerais não-metálicos;
+            químicos; artefatos de madeira; componentes (microeletrônica);
+            dentre outros.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+      </section>
+      <section className="h-screen bg-green-800"></section>
+    </div>
+  );
 }
